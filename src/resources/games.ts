a34656930bce87052fe20f5c6091fecdb9f83a24
hizen <134218727+hizenxyz@users.noreply.github.com>
2025-06-26 19:48:49 -0400

    refactor: update games resource to align with battlemetrics documentation

--- src/resources/games.ts
+++ src/resources/games.ts
@@ -2,11 +2,17 @@ import { getClient } from "../client";
 import { flattenParams } from "../utils/flattenParams";
 import { GameListParams } from "../types/gamesParams";
 
-export async function listGames(params?: GameListParams) {
+export async function getGameList(params?: GameListParams) {
   const client = getClient();
   const flat = params ? flattenParams(params) : undefined;
   const res = await client.get("/games", {
     params: flat,
   });
+  return res.data;
+}
+
+export async function getGameInfo(gameId: string) {
+  const client = getClient();
+  const res = await client.get(`/games/${gameId}`);
   return res.data;
 }
